e17447181c93b58e42841e677844ecd231de4a91
DehaniPathirana <dehanikavindya3@gmail.com>
2024-07-24 13:05:10 +0530

    create bookingRepository class

--- PhegonHotel/src/main/java/com/example/api/repository/BookingRepository.java
+++ PhegonHotel/src/main/java/com/example/api/repository/BookingRepository.java
@@ -3,5 +3,8 @@ package com.example.api.repository;
 import com.example.api.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
 }
